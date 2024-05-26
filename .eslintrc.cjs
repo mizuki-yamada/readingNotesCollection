5272fd1c9bd7018eb7208068b8c7d6e9686a7386
mizuki.yamada <mizuki.yamada@e-seikatsu.co.jp>
2024-05-26 19:15:39 +0900

    refs#1 環境構築

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', "plugin:react/recommended",'plugin:storybook/recommended'],
+  overrides:[],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+  },
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# readingNotesCollection
+
+- React+Typescript+firebaseの練習用アプリ
+
+## 要件など
+-  ようこそページ
+-  本を表示するページ
+-  記事を表示するページ
+-  認証を入れる
+-  登録した日、情報を更新した日を表示する
+-  本、記事のデータはIDで管理したい
+-  感想とか学びとかを入力、表示したい
+-  ラベルで管理して、ソートできるようにしたい
+-  検索を入れてもいいかも
+
+使用技術
+-  React
+-  TypeScript
+-  Vite
+-  MUI
+-  Firebase
+-  ホスティングはgithubpages(考えることが少なそうなので)
+-  
+
+## ホスト先
+- https://my-reading-notes-collection.web.app/
